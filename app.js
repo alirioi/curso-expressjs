88020cb6d6eae017d80f508d0b8f33ad4196f49c
Alirio Isea <isealirio@gmail.com>
2025-11-27 20:48:18 -0400

    Agregar archivo .env-example y actualizar app.js para usar la variable de entorno PORT

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`
+    <h1>Curso de Express.js</h1>
+    <p>Esto es una aplicación Node.js con Express.js.</p>
+    <p>Corre en el puerto ${PORT}</p>
+    `);
 });
 
 app.listen(PORT, () => {
